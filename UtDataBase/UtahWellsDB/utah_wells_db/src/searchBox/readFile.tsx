ccf31b2c0d22cdb1a5a7683e964c2470b367f368
lxu363 <qsxulei@gmail.com>
2022-06-13 08:53:00 -0600

    csvTojson fail

--- UtDataBase/UtahWellsDB/utah_wells_db/src/searchBox/readFile.tsx
+++ UtDataBase/UtahWellsDB/utah_wells_db/src/searchBox/readFile.tsx
@@ -1,8 +1,16 @@
 export default async function readToJSON(fileName: String){
     console.log('start to json');
     const csv=require('csvtojson');
+    const fs=require('fs');
 
-    const jsonArray=await csv().fromFile(fileName);
+    fs.createReadStream(fileName)
+  .pipe(csv())
+  .on(fileName, (row: any) => {
+    console.log(row);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+  });
     console.log("done to json");
-    return jsonArray;
+    return "jsonArray";
 }
